11cc6749aeb829c2f7565bee6dab144e3e2516c8
Maria2721 <mashaivvas@gmail.com>
2026-05-17 20:23:16 +0300

    refactor: convert SearchSection to functional component

--- src/components/SearchSection/SearchSection.tsx
+++ src/components/SearchSection/SearchSection.tsx
@@ -1,17 +1,13 @@
-import { Component } from 'react';
-import type { SearchProps } from '../../ts/interfaces';
 import { Search } from '../Search/Search';
 
-export class SearchSection extends Component<SearchProps> {
-  render() {
-    const { value, onChange, onSearch } = this.props;
+import type { SearchProps } from '../../ts/interfaces';
 
-    return (
-      <section className="bg-white p-5 rounded-2xl shadow-md border border-gray-100">
-        <h2 className="text-lg font-semibold text-gray-800 mb-3">⚡ Search</h2>
+export function SearchSection({ value, onChange, onSearch }: SearchProps) {
+  return (
+    <section className="bg-white p-5 rounded-2xl shadow-md border border-gray-100">
+      <h2 className="text-lg font-semibold text-gray-800 mb-3">⚡ Search</h2>
 
-        <Search value={value} onChange={onChange} onSearch={onSearch} />
-      </section>
-    );
-  }
+      <Search value={value} onChange={onChange} onSearch={onSearch} />
+    </section>
+  );
 }
